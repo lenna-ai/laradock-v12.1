4af7d7c6477dd72d90d50a86c5d8192c4f9d2dc1
tonilenna <toni@lenna.ai>
2022-11-03 10:50:32 +0700

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'v12.1',
+        'version' => '12.1.0.0',
+        'reference' => 'ba71f06eac85dc423ccd5842c5ed8477a6aedd89',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'v12.1',
+            'version' => '12.1.0.0',
+            'reference' => 'ba71f06eac85dc423ccd5842c5ed8477a6aedd89',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
